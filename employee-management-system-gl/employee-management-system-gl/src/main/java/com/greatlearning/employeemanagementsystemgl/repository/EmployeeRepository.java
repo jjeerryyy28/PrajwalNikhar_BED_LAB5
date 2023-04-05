a4d0478855a8f148039796ce11c446ef76ae7a9a
Prajwal Nikhar <83206495+jjeerryyy28@users.noreply.github.com>
2023-04-06 00:49:38 +0530

    Used Spring boot to create an employee management system

--- employee-management-system-gl/employee-management-system-gl/src/main/java/com/greatlearning/employeemanagementsystemgl/repository/EmployeeRepository.java
+++ employee-management-system-gl/employee-management-system-gl/src/main/java/com/greatlearning/employeemanagementsystemgl/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greatlearning.employeemanagementsystemgl.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
